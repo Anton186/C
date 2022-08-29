ab83dc42708a57be05b71511d37d57888c8c53ef
Чернышев Антон <black2604@mail.ru>
2022-08-29 11:33:45 +0500

    Пример 5 добавлен

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "антон")
+{
+    Console.WriteLine ("Ура это же самый крутой человек на свете!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
