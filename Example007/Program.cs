b9e28cbe5c8561c732abceb3a58aa6015027ab6b
Чернышев Антон <black2604@mail.ru>
2022-08-30 10:06:01 +0500

    всё збс

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -7,10 +7,10 @@ int xa = 40, ya = 1,
     xc = 80, yc = 30;
 
 Console.SetCursorPosition (xa, ya);
-Console.WriteLine("+");
+Console.WriteLine("*");
 
 Console.SetCursorPosition (xb, yb);
-Console.WriteLine("+");
+Console.WriteLine("=");
 
 Console.SetCursorPosition (xc, yc);
 Console.WriteLine("+");
@@ -20,28 +20,28 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while(count < 10)
+while(count < 115500)
 {
     int what = new Random() .Next(0, 3);
     if (what == 0)
     {
-        x = (x + xa ) / 2;
-        y = (y + ya ) / 2;
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
     }
     
     if (what == 1)
     {
-        x = (x + xb) /2;
-        y = (x + yb) /2;
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
     }
 
     if (what == 2)
     {
-        x = (x + xc) /2;
-        y = (x + yc) /2;
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
     }
 
     Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
-    count = count + 1
+    Console.WriteLine("=");
+    count ++;
 }
